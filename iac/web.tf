27901cd629347b8a9326b9de59a9189f4fddf286
Darli963 <dmedinas5@upao.edu.pe>
2026-04-15 14:37:39 +0000

    feat: agregar contenedor mysql usando variables por entorno

---
+++ iac/web.tf
@@ -0,0 +1,33 @@
+resource "docker_container" "web" {
+  name  = "web-${terraform.workspace}-01"
+  image = "lab/web"
+
+  ports {
+    internal = "80"
+    external = var.web_port[terraform.workspace]
+  }
+}
+
+resource "docker_container" "api" {
+  name  = "api-${terraform.workspace}-01"
+  image = "lab/api"
+
+  ports {
+    internal = "3000"
+    external = var.api_port[terraform.workspace]
+  }
+}
+
+resource "docker_container" "db" {
+  name  = "db-${terraform.workspace}-01"
+  image = "mysql:8"
+
+  env = [
+    "MYSQL_ROOT_PASSWORD=root"
+  ]
+
+  ports {
+    internal = "3306"
+    external = var.db_port[terraform.workspace]
+  }
+}
